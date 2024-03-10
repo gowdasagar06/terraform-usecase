28937affb7df67de9e44a000e7b652cdfaab5600
gowdasagar06 <92599815+gowdasagar06@users.noreply.github.com>
2024-03-11 01:42:20 +0530

    Update main.tf

--- Copy remote local/main.tf
+++ Copy remote local/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2" {
   ami           = "ami-0287a05f0ef0e9d9a"
   instance_type = "t2.micro"
-
+  associate_public_ip_address = true
   vpc_security_group_ids = ["sg-0b5c4ec4808949635"] 
   subnet_id              = "subnet-05feb5f6a1540579a"  
   key_name = "gowda"
